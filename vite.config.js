f0e87797df4ff82117d5b49bc7e79a1fe0e16796
pph <pph>
2024-12-02 00:23:31 +0630

    commented outDir

--- vite.config.js
+++ vite.config.js
@@ -14,7 +14,7 @@ export default defineConfig({
         }),
     ],
     build: {
-        outDir: 'public/build',
+        // outDir: 'public/build',
         manifest: true,
         emptyOutDir: true,
         rollupOptions: {
