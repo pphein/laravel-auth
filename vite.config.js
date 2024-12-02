546d64d2cd468b769a6324a38f6eb785beeeae35
pph <pph>
2024-12-02 23:22:50 +0630

    added vite build

--- vite.config.js
+++ vite.config.js
@@ -7,14 +7,20 @@ export default defineConfig({
             input: [
                 'resources/css/app.css',
                 'resources/js/app.js',
-                'resources/sass/app.scss',
-                'resources/js/app.js',
+                'resources/sass/app.scss'
             ],
             refresh: true,
         }),
     ],
     build: {
+        manifest: true,
         outDir: 'public/build',
-        manifest: true
+        rollupOptions: {
+            output: {
+                chunkFileNames: 'assets/[name]-[hash].js',
+                entryFileNames: 'assets/[name]-[hash].js',
+                assetFileNames: 'assets/[name]-[hash].[ext]',
+            },
+        },
     },
 });
