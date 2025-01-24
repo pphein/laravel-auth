28ba6ded2c5da440c45b6cc7469e375fd97456fb
pph <pph>
2025-01-24 17:26:11 +0630

    added login css in vite

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,8 @@ export default defineConfig({
                 'resources/css/app.css',
                 'resources/js/app.js',
                 'resources/sass/app.scss',
-                'resources/css/custom.css'
+                'resources/css/custom.css',
+                'resources/css/login.css'
             ],
             refresh: true,
         }),
