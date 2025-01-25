226047f0245f2f384b9a023224d377151b83cc58
pph <pph>
2025-01-25 09:25:34 +0630

    Refactored login and register page

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container vh-100">
+<div class="container min-vh-100">
     <div class="row">
         <!-- <div class="col-md-8">
             <div class="card">
